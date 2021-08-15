27dc747a34cac7f8b049d5b0c6ddb7590100d6ec
miguelvr98 <miguelvr98@gmail.com>
2021-08-15 11:51:13 +0200

    Configurando proyecto

--- SheatCreator/SheatCreator/settings.py
+++ SheatCreator/SheatCreator/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'SheatCreator.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'SheatCreator',
+
+        'USER': 'usuario',
+
+        'PASSWORD': 'usuario',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
