58c7f451a3428ff7c4f038380c54f82b5a3b686f
miguelvr98 <miguelvr98@gmail.com>
2021-11-14 23:42:03 +0100

    Pequeños cambios al modelo de dominio y comienzo del populate.xml

--- SheatCreator/main/models.py
+++ SheatCreator/main/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
     nickname = models.TextField(verbose_name='Nickname')
-    esAdmin = models.BooleanField(verbose_name='Admin', default=False)
+    es_admin = models.BooleanField(verbose_name='Admin', default=False)
 
     def __str__(self):
         return self.nickname
@@ -30,13 +30,13 @@ class Personaje(models.Model):
     alineamiento = models.CharField(verbose_name='alineamiento', 
     max_length=2, choices=Alineamiento.choices, null=True)
     carga = models.TextField(verbose_name='Carga', null=True)
-    puntosDeGolpe = models.IntegerField(verbose_name='Puntos de golpe',
+    puntos_de_golpe = models.IntegerField(verbose_name='Puntos de golpe',
     null=True)
-    resistenciaDano = models.IntegerField(verbose_name='Resistencia al daño',
+    resistencia_dano = models.IntegerField(verbose_name='Resistencia al daño',
     null=True)
-    resistenciaConjuros = models.IntegerField(verbose_name='Resistencia a Conjuros',
+    resistencia_conjuros = models.IntegerField(verbose_name='Resistencia a Conjuros',
     null=True)
-    claseArmadura = models.IntegerField(verbose_name='Clase de armadura',
+    clase_armadura = models.IntegerField(verbose_name='Clase de armadura',
     default=10)
     desprevenido = models.IntegerField(verbose_name='Desprevenido',
     default=10)
@@ -54,16 +54,18 @@ class Personaje(models.Model):
     sabiduria = models.IntegerField(verbose_name='Sabiduría',
      default=10)
     carisma = models.IntegerField(verbose_name='Carisma', default=10)
+    es_publico = models.BooleanField(verbose_name='Es público', default=False)
     idiomas = models.ManyToManyField('Idioma')
     inmune = models.ManyToManyField('Inmune')
     raza = models.ForeignKey('Raza', on_delete=models.CASCADE, null=True)
     clase = models.ManyToManyField('Clase')
     dotes = models.ManyToManyField('Dote')
-    puntuacionesHabilidad = models.ManyToManyField('PuntuacionHabilidad')
-    inventario = models.ForeignKey('Inventario', on_delete=models.CASCADE, null=True)
-    companeroAnimal = models.ForeignKey('CompaneroAnimal', on_delete=models.CASCADE, null=True)
+    puntuaciones_habilidad = models.ManyToManyField('PuntuacionHabilidad')
+    inventario_equipado = models.ForeignKey('InventarioEquipado', on_delete=models.CASCADE, null=True)
+    inventario_no_equipado = models.ForeignKey('InventarioNoEquipado', on_delete=models.CASCADE, null=True)
+    companero_animal = models.ForeignKey('CompaneroAnimal', on_delete=models.CASCADE, null=True)
     objetos = models.ManyToManyField('Objeto')
-
+    conjuros_conocidos = models.ManyToManyField('Conjuro')
 
     def BonificadorFuerza(self):
         return int((fuerza-10)/2)
@@ -117,7 +119,7 @@ class Personaje(models.Model):
         inteligencia = models.IntegerField(verbose_name='Inteligencia', null=True)
         sabiduria = models.IntegerField(verbose_name='Sabiduría', null=True)
         carisma = models.IntegerField(verbose_name='Carisma', null=True)
-        bonificacionRaza = models.ManyToManyField('BonificacionRaza')
+        bonificacion_raza = models.ManyToManyField('BonificacionRaza')
         idiomas = models.ManyToManyField('Idioma')
 
         def __str__(self):
@@ -160,22 +162,23 @@ class Personaje(models.Model):
     class Clase(models.Model):
         clase = models.TextField(verbose_name='Clase')
         nivel = models.IntegerField(verbose_name='Nivel', default=1)
-        dadosDeGolpe = models.IntegerField(verbose_name='Dados de golpe',
+        dados_de_golpe = models.IntegerField(verbose_name='Dados de golpe',
         default=6)
-        ataqueBase = models.TextField(verbose_name='Ataque base',
+        ataque_base = models.TextField(verbose_name='Ataque base',
          null=False)
         fortaleza = models.IntegerField(verbose_name='Fortaleza', 
         null=False)
         reflejos = models.IntegerField(verbose_name='Reflejos', null=False)
         voluntad = models.IntegerField(verbose_name='Fortaleza', null=False)
-        rafagaDeGolpes = models.TextField(verbose_name='Ráfaga de golpes', null=True)
-        danoDesarmado = models.TextField(verbose_name='Daño desarmado', null = True)
-        bonificacionAc = models.IntegerField(verbose_name='Bonificación AC', null = True)
-        movimientoRapido = models.IntegerField(verbose_name='Movimiento rápido', null =True)
-        nivelConjuroDiario = models.ManyToManyField('NivelConjuroDiario')
+        competencia = models.TextField(verbose_name='Competente con', null=True)
+        rafaga_de_golpes = models.TextField(verbose_name='Ráfaga de golpes', null=True)
+        dano_desarmado = models.TextField(verbose_name='Daño desarmado', null = True)
+        bonificacion_ac = models.IntegerField(verbose_name='Bonificación AC', null = True)
+        movimiento_rapido = models.IntegerField(verbose_name='Movimiento rápido', null =True)
+        nivel_conjuro_diario = models.ManyToManyField('NivelConjuroDiario')
         poderes = models.ManyToManyField('Poder')
         especiales = models.ManyToManyField('Especial')
-        companeroAnimal = models.ManyToManyField('CompaneroAnimal', default=False)
+        companero_animal = models.ManyToManyField('CompaneroAnimal', default=False)
         habilidad = models.ManyToManyField('Habilidad')
 
         def __str__(self):
@@ -189,15 +192,16 @@ class Personaje(models.Model):
         descripcion = models.TextField(verbose_name='Descripcion')
         tipo = models.TextField(verbose_name='Tipo', null=True)
         nivel = models.IntegerField(verbose_name='Nivel', null=True)
-        ataqueBase = models.IntegerField(verbose_name='Ataque base', null=True)
+        ataque_base = models.IntegerField(verbose_name='Ataque base', null=True)
         fuerza = models.IntegerField(verbose_name='Fuerza', null=True)
         destreza = models.IntegerField(verbose_name='Destreza', null=True)
         constitucion = models.IntegerField(verbose_name='Constitución', null=True)
         inteligencia = models.IntegerField(verbose_name='Inteligencia', null=True)
         sabiduria = models.IntegerField(verbose_name='Sabiduría', null=True)
         carisma = models.IntegerField(verbose_name='Carisma', null=True)
-        esDoteCompaneroAnimal = models.BooleanField(verbose_name='Es dote de compañero animal', default=False)
-        prerrequisitoDote = models.ManyToManyField('self')
+        creador = models.OneToOneField('Perfil', null=True, on_delete=models.SET_NULL)
+        es_dote_companero_animal = models.BooleanField(verbose_name='Es dote de compañero animal', default=False)
+        prerrequisito_dote = models.ManyToManyField('self')
     
         def __str__(self):
             return self.nombre
@@ -209,13 +213,13 @@ class Personaje(models.Model):
         nombre = models.TextField(verbose_name='Nombre')
         nivel = models.IntegerField(verbose_name='Nivel')
         escuela = models.TextField(verbose_name='Escuela')
-        tiempoDeLanzamiento = models.TextField(verbose_name='Tiempo de lanzamiento')
+        tiempo_de_lanzamiento = models.TextField(verbose_name='Tiempo de lanzamiento')
         alcance = models.TextField(verbose_name='Alcance')
         efecto = models.TextField(verbose_name='Efecto', null=True)
         objetivo = models.TextField(verbose_name='Objetivo', null=True)
         duracion = models.TextField(verbose_name='Duración')
-        tiroDeSalvacion = models.BooleanField(verbose_name='Tiro de salvación')
-        resistenciaConjuros = models.BooleanField(verbose_name='Resistencia a conjuros')
+        tiro_de_salvacion = models.BooleanField(verbose_name='Tiro de salvación')
+        resistencia_conjuros = models.BooleanField(verbose_name='Resistencia a conjuros')
         descripcion = models.TextField(verbose_name='Descripción')
 
         def __str__(self):
@@ -228,14 +232,14 @@ class Personaje(models.Model):
         nombre = models.TextField(verbose_name='Nombre')
         descripcion = models.TextField(verbose_name='Descripción')
         nivel = models.IntegerField(verbose_name='Nivel', null=True)
-        ataqueBase = models.IntegerField(verbose_name='Ataque base', null=True)
+        ataque_base = models.IntegerField(verbose_name='Ataque base', null=True)
         fuerza = models.IntegerField(verbose_name='Fuerza', null=True)
         destreza = models.IntegerField(verbose_name='Destreza', null=True)
         constitucion = models.IntegerField(verbose_name='Constitución', null=True)
         inteligencia = models.IntegerField(verbose_name='Inteligencia', null=True)
         sabiduria = models.IntegerField(verbose_name='Sabiduría', null=True)
         carisma = models.IntegerField(verbose_name='Carisma', null=True)
-        prerrequisitoPoder = models.ManyToManyField('self')
+        prerrequisito_poder = models.ManyToManyField('self')
 
         def __str__(self):
             return nombre
@@ -256,7 +260,7 @@ class Personaje(models.Model):
         nombre = models.TextField(verbose_name='Nombre')
         descripcion = models.TextField(verbose_name='Descripción')
         nivel = models.IntegerField(verbose_name='Nivel', null=True)
-        esEspecialCompaneroAnimal = models.BooleanField(verbose_name='Es especial de compañero animal')
+        es_especial_companero_animal = models.BooleanField(verbose_name='Es especial de compañero animal')
 
         def __str__(self):
             return nombre
@@ -267,12 +271,12 @@ class Personaje(models.Model):
     class CompaneroAnimal(models.Model):
         nombre = models.TextField(verbose_name='Nombre')
         nivel = models.IntegerField(verbose_name='Nivel')
-        dadosDeGolpe = models.IntegerField(verbose_name='Dados de golpe')
-        puntosDeGolpe = models.IntegerField(verbose_name='Puntos de golpe')
+        dados_de_golpe = models.IntegerField(verbose_name='Dados de golpe')
+        puntos_de_golpe = models.IntegerField(verbose_name='Puntos de golpe')
         tamano = models.TextField(verbose_name='Tamaño')
         velocidad = models.TextField(verbose_name='Velocidad')
         ca = models.IntegerField(verbose_name='Clase de armadura')
-        ataqueBase = models.IntegerField(verbose_name='Ataque base')
+        ataque_base = models.IntegerField(verbose_name='Ataque base')
         ataque = models.TextField(verbose_name='Ataque')
         fuerza = models.IntegerField(verbose_name='Fuerza')
         destreza = models.IntegerField(verbose_name='Destreza')
@@ -286,7 +290,7 @@ class Personaje(models.Model):
         dotes = models.ManyToManyField('Dote')
         especial = models.ManyToManyField('Especial')
         trucos = models.ManyToManyField('Truco')
-        puntuacionHabilidad = models.ManyToManyField('PuntuacionHabilidadCA')
+        puntuacion_habilidad = models.ManyToManyField('PuntuacionHabilidadCA')
 
         def __str__(self):
             return nombre
@@ -298,7 +302,7 @@ class Personaje(models.Model):
         nombre = models.TextField(verbose_name='Nombre')
         descripcion = models.TextField(verbose_name='Descripción')
         cd = models.IntegerField(verbose_name='CD')
-        prerrequisitoTruco = models.ManyToManyField('self')
+        prerrequisito_truco = models.ManyToManyField('self')
 
         def __str__(self):
             return nombre
@@ -316,8 +320,11 @@ class Personaje(models.Model):
         class Meta:
             ordering = ('pk', )
 
-    class Inventario(models.Model):
-        equipado = models.BooleanField(verbose_name='Está equipado')
+    class InventarioEquipado(models.Model):
+        objetos = models.ManyToManyField('Objeto')
+
+    class InventarioNoEquipado(models.Model):
+        objetos = models.ManyToManyField('Objeto')
 
     class Objeto(models.Model):
         clase = models.TextField(verbose_name='Clase', null=True)
@@ -333,8 +340,8 @@ class Personaje(models.Model):
             ordering = ('nombre', )
     
     class Arma(Objeto):
-        danoP = models.TextField(verbose_name='Daño P')
-        danoM = models.TextField(verbose_name='Daño M')
+        dano_p = models.TextField(verbose_name='Daño P')
+        dano_m = models.TextField(verbose_name='Daño M')
         critico = models.TextField(verbose_name='Crítico')
         alcance = models.TextField(verbose_name='Alcance', null=True)
         tipo = models.TextField(verbose_name='Tipo')
@@ -347,12 +354,12 @@ class Personaje(models.Model):
             ordering = ('nombre', )
 
     class Armadura(Objeto):
-        bonifArm = models.IntegerField(verbose_name='Bonificación armadura')
-        bonifMaxDes = models.IntegerField(verbose_name='Bonificación máximo destreza')
-        penalizArm = models.IntegerField(verbose_name='Penalizador armadura')
-        falloConjArc = models.IntegerField(verbose_name='Fallo conjuro arcano')
-        velocidad9m = models.IntegerField(verbose_name='Velocidad 9m')
-        velocidad6m = models.IntegerField(verbose_name='Velocidad 6m')
+        bonif_arm = models.IntegerField(verbose_name='Bonificación armadura')
+        bonif_max_des = models.IntegerField(verbose_name='Bonificación máximo destreza')
+        penaliz_arm = models.IntegerField(verbose_name='Penalizador armadura')
+        fallo_conj_arc = models.IntegerField(verbose_name='Fallo conjuro arcano')
+        velocidad_9m = models.IntegerField(verbose_name='Velocidad 9m')
+        velocidad_6m = models.IntegerField(verbose_name='Velocidad 6m')
 
         def __str__(self):
             return self.nombre
@@ -364,7 +371,7 @@ class Personaje(models.Model):
         nombre = models.TextField(verbose_name='Nombre')
         descripcion = models.TextField(verbose_name='Descripción')
         coste = models.IntegerField(verbose_name='Coste', null=True)
-        costeDinero = models.IntegerField(verbose_name='Coste dinero', null=True)
+        coste_dinero = models.IntegerField(verbose_name='Coste dinero', null=True)
 
         def __str__(self):
             return self.nombre
